3f95721c97703933b551b87f6c0a3bbde19e38bd
TuanNghia96 <nghia.nbt143167@sis.hust.edu.vn>
2020-06-14 18:13:20 +0700

    fix/about

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -22,17 +22,18 @@
         <div class="row">
             <div class="col-12 col-md-6 col-lg-4">
                 <div class="contact-location-details">
-                    <h2 class="entry-title">Ha Noi</h2>
+                    <h2 class="entry-title">Hà Nội</h2>
 
                     <div class="entry-content">
-                        <p>Vestibulum eget lacus at mauris sagittis varius. Etiam ut venenatis dui. Nullam tellus risus, pellentesque at facilisis et.</p>
+                        <p>Hiện nay, Tổng công ty Agenda có địa điểm làm việc ở 3 nơi. Trong đó, Hà nội là chủ sở chính chịu trách nhiệm kinh doanh toàn bộ các dịch vụ do Tổng công ty cung cấp.</p>
+                        <br>
                     </div>
 
                     <footer class="entry-footer">
                         <ul>
-                            <li class="contact-address">667889 Madison Avenue, no24-56</li>
-                            <li class="contact-number">665 5667 8899 661</li>
-                            <li class="contact-email"><a href="#">office@yourbusiness.com</a></li>
+                            <li class="contact-address">1 Đại Cồ Việt, Bách Khoa, Hai Bà Trưng, Hà Nội, Việt Nam</li>
+                            <li class="contact-number">+84 667 889 661</li>
+                            <li class="contact-email"><a href="#">HN_Agenda@gmail.com</a></li>
                         </ul>
                     </footer>
                 </div>
@@ -40,17 +41,17 @@
 
             <div class="col-12 col-md-6 col-lg-4">
                 <div class="contact-location-details">
-                    <h2 class="entry-title">Da Nang</h2>
+                    <h2 class="entry-title">Đà Nẵng</h2>
 
                     <div class="entry-content">
-                        <p>Vestibulum eget lacus at mauris sagittis varius. Etiam ut venenatis dui. Nullam tellus risus, pellentesque at facilisis et.</p>
+                        <p>Với chi nhánh đặt ở Đà Nẵng, công ty Agenda Đà Nẵng đã và đang mở rộng phạm vi phát triển hơn nữa. Đây là chi nhánh quan trọng giúp cho các doanh nghiệp tiềm năng tại đây có được sự hỗ trợ tốt nhất.</p>
                     </div>
 
                     <footer class="entry-footer">
                         <ul>
-                            <li class="contact-address">122344 Island Str, no23</li>
-                            <li class="contact-number">665 5667 8899 661</li>
-                            <li class="contact-email"><a href="#">office@yourbusiness.com</a></li>
+                            <li class="contact-address">Nguyễn Văn Linh, Hòa Thuận Tây, Hải Châu, Đà Nẵng 550000, Việt Nam</li>
+                            <li class="contact-number">+84 667 599 661</li>
+                            <li class="contact-email"><a href="#">DN_Agenda@gmail.com</a></li>
                         </ul>
                     </footer>
                 </div>
@@ -58,17 +59,18 @@
 
             <div class="col-12 col-md-6 col-lg-4">
                 <div class="contact-location-details">
-                    <h2 class="entry-title">Sai gon</h2>
+                    <h2 class="entry-title">Sài gòn</h2>
 
                     <div class="entry-content">
-                        <p>Vestibulum eget lacus at mauris sagittis varius. Etiam ut venenatis dui. Nullam tellus risus, pellentesque at facilisis et.</p>
+                        <p>Với chi nhánh đặt ở tp. Hồ Chí Minh, đây là nơi chịu trách nhiệm kinh doanh toàn bộ kế hoạch phát triển của Tổng Công ty trên địa bàn thành phố Hà Nội.</p>
+                        <br>
                     </div>
 
                     <footer class="entry-footer">
                         <ul>
-                            <li class="contact-address">667889 Long Avenue, no24-56</li>
-                            <li class="contact-number">665 5667 8899 661</li>
-                            <li class="contact-email"><a href="#">office@yourbusiness.com</a></li>
+                            <li class="contact-address">Departure Loop, Phường 2, Tân Bình, Hồ Chí Minh, Việt Nam</li>
+                            <li class="contact-number">+84 665 566 899</li>
+                            <li class="contact-email"><a href="#">HCM_Agenda@gmail.com</a></li>
                         </ul>
                     </footer>
                 </div>
