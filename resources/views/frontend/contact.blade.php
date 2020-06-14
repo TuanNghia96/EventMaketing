6388afc91f96fb1ea352f907899bef63e716aa21
TuanNghia96 <nghia.nbt143167@sis.hust.edu.vn>
2020-06-14 18:17:41 +0700

    fix/contact

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -4,54 +4,61 @@
 
 @section('background_header')
     <div class="page-header contact-page-header">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <header class="entry-header">
-                    <h1 class="entry-title">Liên hệ.</h1>
-                </header>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <header class="entry-header">
+                        <h1 class="entry-title">Liên hệ.</h1>
+                    </header>
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-12">
-            <div class="contact-form">
-                <form method="post" class="row" action="{{ route('contact.send') }}">
-                    @csrf
-                    <div class="col-12 col-md-3">
-                        @guest()
-                            <input type="text" id="fname" name="name" class="form-control" placeholder="Name" value="{{ old('name') }}" required>
-                        @else
-                            <input type="text" id="fname" name="name" class="form-control" placeholder="Name" value="{{ Auth::user()->name }}" readonly required>
-                        @endguest
-                    </div>
-                    <div class="col-12 col-md-3">
-                        @guest()
-                            <input type="text" id="" name="phone" class="form-control" placeholder="Phone number" value="{{ old('phone') }}">
-                        @else
-                            <input type="text" id="" name="phone" class="form-control" placeholder="Phone number" value="{{ Auth::user()->user->phone }}" readonly>
-                        @endguest
-                    </div>
-                    <div class="col-12 col-md-3">
-                        @guest()
-                            <input type="text" id="email" name="email" class="form-control" placeholder="Email" value="{{ old('email') }}">
-                        @else
-                            <input type="text" id="email" name="email" class="form-control" placeholder="Email" value="{{ Auth::user()->email }}" readonly>
-                        @endguest
-                    </div>
-                    <div class="col-12 col-md-3">
-                        <input type="text" id="subject" name="subject" class="form-control" placeholder="Subject" value="{{ old('subject') }}" required>
-                    </div>
-                    <div class="col-12"><textarea placeholder="Message" name="message" rows="8">{{ old('message') }}</textarea></div>
-                    <div class="col-12 flex justify-content-center"><input class="btn gradient-bg" type="submit" value="Send Message"></div>
-                </form>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="contact-form">
+                    <form method="post" class="row" action="{{ route('contact.send') }}">
+                        @csrf
+                        <div class="col-12 col-md-4">
+                            <label for="">Tên</label>
+                            @guest()
+                                <input type="text" id="fname" name="name" class="form-control" placeholder="Name" value="{{ old('name') }}" required>
+                            @else
+                                <input type="text" id="fname" name="name" class="form-control" placeholder="Name" value="{{ Auth::user()->name }}" readonly required>
+                            @endguest
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <label for="">Số điện thoại</label>
+                            @guest()
+                                <input type="text" id="" name="phone" class="form-control" placeholder="Phone number" value="{{ old('phone') }}">
+                            @else
+                                <input type="text" id="" name="phone" class="form-control" placeholder="Phone number" value="{{ Auth::user()->user->phone }}" readonly>
+                            @endguest
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <label for="">Địa chỉ mail</label>
+                            @guest()
+                                <input type="text" id="email" name="email" class="form-control" placeholder="Email" value="{{ old('email') }}">
+                            @else
+                                <input type="text" id="email" name="email" class="form-control" placeholder="Email" value="{{ Auth::user()->email }}" readonly>
+                            @endguest
+                        </div>
+                        <div class="col-12">
+                            <label for="">Tiêu đề</label>
+                            <input type="text" id="subject" name="subject" class="form-control" placeholder="Subject" value="{{ old('subject') }}" required>
+                        </div>
+                        <div class="col-12">
+                            <label for="">Nội dung</label>
+                            <textarea placeholder="Message" name="message" rows="8">{{ old('message') }}</textarea>
+                        </div>
+                        <div class="col-12 flex justify-content-center"><input class="btn gradient-bg" type="submit" value="Gửi tin nhắn"></div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
