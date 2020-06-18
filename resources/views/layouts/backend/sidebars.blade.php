490a53bfe4a86a9d92ca7581b25cdb83fc7e11a8
TuanNghia96 <nghia.nbt143167@sis.hust.edu.vn>
2020-06-18 10:45:56 +0700

    fix/sidebars

--- resources/views/layouts/backend/sidebars.blade.php
+++ resources/views/layouts/backend/sidebars.blade.php
@@ -39,7 +39,7 @@
                 </li>
                 <li class="nav-item">
                     <a data-toggle="collapse" href="#base">
-                        <i class="fas fa-cube"></i>
+                        <i class="fas fa-calendar"></i>
                         <p>Quản lý sự kiện</p>
                         <span class="caret"></span>
                     </a>
@@ -47,73 +47,63 @@
                         <ul class="nav nav-collapse">
                             <li>
                                 <a href="{{ route('events.index') }}">
-                                    <span class="sub-item">Danh mục sự kiện</span>
+                                    <span class="sub-item">
+                                        <i class="fas fa-calendar-alt"></i>
+                                        Danh mục sự kiện</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="{{ route('events.validated') }}">
-                                    <span class="sub-item">Sự kiện đang diễn ra</span>
+                                    <span class="sub-item">
+                                        <i class="fas fa-calendar-check"></i>
+                                        Sự kiện đang diễn ra</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="{{ route('events.waiting') }}">
-                                    <span class="sub-item">Sự kiện đang chờ</span>
+                                    <span class="sub-item">
+                                        <i class="fas fa-calendar-plus"></i>
+                                        Sự kiện đang chờ</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="{{ route('events.delete') }}">
-                                    <span class="sub-item">Sự kiện xin huỷ</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="{{ route('types.index') }}">
-                                    <span class="sub-item">Thể loại sự kiện</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="{{ route('categories.index') }}">
-                                    <span class="sub-item">Danh mục sự kiện</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="{{ route('coupons.index') }}">
-                                    <span class="sub-item">Phiếu giảm giá.</span>
+                                    <span class="sub-item"><i class="fas fa-calendar-times"></i>
+                                        Sự kiện xin huỷ</span>
                                 </a>
                             </li>
                         </ul>
                     </div>
                 </li>
-                {{--<li class="nav-item">
-                    <a data-toggle="collapse" href="#sidebarLayouts">
-                        <i class="fas fa-exchange-alt"></i>
-                        <p>Giao dịch</p>
+                <li class="nav-item">
+                    <a data-toggle="collapse" href="#base">
+                        <i class="fas fa-cube"></i>
+                        <p>Quản lý liên quan</p>
                         <span class="caret"></span>
                     </a>
-                    <div class="collapse" id="sidebarLayouts">
+                    <div class="collapse" id="base">
                         <ul class="nav nav-collapse">
                             <li>
-                                <a href="#">
-                                    <span class="sub-item">Hóa đơn</span>
+                                <a href="{{ route('types.index') }}">
+                                    <span class="sub-item"><i class="fas fa-bookmark"></i>
+                                    Thể loại sự kiện</span>
                                 </a>
                             </li>
                             <li>
-                                <a href="#">
-                                    <span class="sub-item">Nhập hàng</span>
+                                <a href="{{ route('categories.index') }}">
+                                    <span class="sub-item"><i class="fas fa-atlas"></i>
+                                    Danh mục sự kiện</span>
                                 </a>
                             </li>
                             <li>
-                                <a href="#">
-                                    <span class="sub-item">Trả hàng</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <span class="sub-item">Xuất hủy</span>
+                                <a href="{{ route('coupons.index') }}">
+                                    <span class="sub-item"><i class="fas fas fa-cart-plus"></i>
+                                    Phiếu giảm giá.</span>
                                 </a>
                             </li>
                         </ul>
                     </div>
-                </li>--}}
+                </li>
                 <li class="nav-item">
                     <a data-toggle="collapse" href="#forms">
                         <i class="fas fa-male"></i>
@@ -124,12 +114,14 @@
                         <ul class="nav nav-collapse">
                             <li>
                                 <a href="{{ route('buyers.index') }}">
-                                    <span class="sub-item">Khách hàng</span>
+                                    <span class="sub-item"><i class="fas fa-ticket-alt"></i>
+                                    Người mua</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="{{ route('enterprises.index') }}">
-                                    <span class="sub-item">Nhà cung cấp</span>
+                                    <span class="sub-item"><i class="fas fas fa-hands-helping"></i>
+                                    Nhà cung cấp</span>
                                 </a>
                             </li>
                         </ul>
@@ -137,7 +129,7 @@
                 </li>
                 <li class="nav-item">
                     <a data-toggle="collapse" href="#tables">
-                        <i class="fas fa-user"></i>
+                        <i class="fas fa-user-cog"></i>
                         <p>Nhân viên</p>
                         <span class="caret"></span>
                     </a>
@@ -145,12 +137,14 @@
                         <ul class="nav nav-collapse">
                             <li>
                                 <a href="{{ route('admin.index') }}">
-                                    <span class="sub-item">Danh sách</span>
+                                    <span class="sub-item"><i class="fas fa-list"></i>
+                                    Danh sách</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="{{ route('admin.create') }}">
-                                    <span class="sub-item">Tạo mới</span>
+                                    <span class="sub-item"><i class="fas fa-plus"></i>
+                                    Tạo mới</span>
                                 </a>
                             </li>
                         </ul>
@@ -166,12 +160,14 @@
                         <ul class="nav nav-collapse">
                             <li>
                                 <a href="{{ route('chart.event') }}">
-                                    <span class="sub-item">Sự kiện</span>
+                                    <span class="sub-item"><i class="fas fa-chart-area"></i>
+                                    Sự kiện</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="{{ route('chart.calendar') }}">
-                                    <span class="sub-item">Lịch sự kiện</span>
+                                    <span class="sub-item"><i class="far fa-calendar-alt"></i>
+                                    Lịch sự kiện</span>
                                 </a>
                             </li>
                         </ul>
